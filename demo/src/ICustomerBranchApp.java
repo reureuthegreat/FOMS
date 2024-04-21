fbfae2bd6a962f66582f678dd9cc167e46bf84d4
sherlock-999 <123862475+sherlock-999@users.noreply.github.com>
2024-04-21 13:51:10 +0800

    Add files via upload

---
+++ demo/src/ICustomerBranchApp.java
@@ -0,0 +1,4 @@
+package Branch;
+
+public interface ICustomerBranchApp {
+}
---
+++ demo/src/IStaffBranchApp.java
@@ -0,0 +1,7 @@
+package Branch;
+
+public interface IStaffBranchApp {
+	void Display_Order();
+	void Process_Order();
+	void View_New_Order();
+}
