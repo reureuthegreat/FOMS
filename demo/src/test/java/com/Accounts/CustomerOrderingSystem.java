8139ad9a5e643cb4d0efe2c933d9fc9e53cb28ad
Cheokerinos <cheokbryan@gmail.com>
2024-04-25 02:55:58 +0800

    updated updated code

--- demo/src/test/java/com/Accounts/CustomerOrderingSystem.java
+++ demo/src/test/java/com/Accounts/CustomerOrderingSystem.java
@@ -1,11 +1,8 @@
 package com.Accounts;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 import com.Branch.Branch;
-import com.Branch.BranchManagement;
 import com.Branch.BranchSystem;
 import com.Branch.Self_Ordering_Kiosk;
 import com.Cart.IShoppingCart;
@@ -15,24 +12,20 @@ import com.Menu.ICustomerMenu;
 
 import com.Menu.MenuOperator;
 import com.Order.Order;
-
+import java.util.Scanner;
 public class CustomerOrderingSystem {
 	
 	
-	public static void main(String[] args) {
-		BranchManagement branchManagement = new BranchManagement();
-		
-		
-		branchManagement.AddBranch();
-		//branchManagement.AddBranch();
-		
+	public void COS(BranchManagement branchManagement) {
 		Scanner scanner = new Scanner(System.in);
-		
+		int choice;
 		Branch branch;
-		
+
+		int counter = 1;
 		System.out.println("Which KFC would you like to go to?");
 		for (Branch br : branchManagement.getBranchList()) {
-			System.out.println(" - " + br.getBranchName());
+			System.out.println(counter + ":" + br.getBranchName());
+			counter++;
 		}
 		
         do {
@@ -51,54 +44,60 @@ public class CustomerOrderingSystem {
 
         //1. Connect to branchSystem
         BranchSystem branchSystem = branch.branchSystem;
-        /*TEMP - TESTING ONLY*/ branchSystem.Branch_Menu_Management();
-        
-       
-        while (true) {
-        	System.out.println("");
-        	System.out.println("");
-        	System.out.println("");
-        	System.out.println("");
-        	
-	        System.out.println("Welcome to Kentucky Fried Children @" + branch.getBranchName() + "!");
-	        System.out.println("+--------------------------------------+");
-	        System.out.println("| I'm NOT lovin it...                  |");
-	        System.out.println("|                                      |");
-	        System.out.println("| Go to:                               |");
-	        System.out.println("|  1. Self-Ordering Kiosk              |");
-	        System.out.println("|  2. Order Collection Site            |");
-	        System.out.println("+--------------------------------------+");
+        /*TEMP - TESTING ONLYbranchSystem.Branch_Menu_Management();*/
 
-       
-	        System.out.print("Enter your choice (1 or 2): ");
-	        int choice = scanner.nextInt();
-	
-	        // Process user's choice
-	        switch (choice) {
-	            case 1:
-	                System.out.println("Going to Self-Ordering Kiosk...");
-	                branch.kiosk_machine.ordering(branchSystem);
-	                break;
-	            case 2:
-	                System.out.println("Going to Order Collection Site...");
-	                branch.collection_Site.Display_Orders(branchSystem);
-	                
-	                System.out.println("Collect Order now?");
-	                System.out.println("+-----------+");
-	                System.out.println("|  1. Yes   |");
-	                System.out.println("|  2. No    |");
-	                System.out.println("+-----------+");
-	                
-	                choice = scanner.nextInt();
-	                
-	                if (choice == 1)
-	                	branch.collection_Site.collect_order(branchSystem);
-	                break;
-	            default:
-	                System.out.println("Fun fact: KFC tastes better than BURGER QUEEN...");
-	                break;
-	        }
-	     }
+			try {
+				Boolean IsValid = true;
+				do {
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+
+			System.out.println("Welcome to Kentucky Fried Children @" + branch.getBranchName() + "!");
+			System.out.println("+--------------------------------------+");
+			System.out.println("| I'm NOT lovin it...                  |");
+			System.out.println("|                                      |");
+			System.out.println("| Go to:                               |");
+			System.out.println("|  1. Self-Ordering Kiosk              |");
+			System.out.println("|  2. Order Collection Site            |");
+			System.out.println("+--------------------------------------+");
+					System.out.print("Enter your choice (1 or 2): ");
+					choice = scanner.nextInt();
+					// Process user's choice
+					switch (choice) {
+						case 1:
+							System.out.println("Going to Self-Ordering Kiosk...");
+							branch.kiosk_machine.ordering(branchSystem);
+							break;
+						case 2:
+							System.out.println("Going to Order Collection Site...");
+							branch.collection_Site.Display_Orders(branchSystem);
+							try{
+									System.out.println("Collect Order now?");
+									System.out.println("+-----------+");
+									System.out.println("|  1. Yes   |");
+									System.out.println("|  2. No    |");
+									System.out.println("+-----------+");
+									choice = scanner.nextInt();
+									if (choice == 1) {
+										branch.collection_Site.collect_order(branchSystem);
+									}
+							}catch(InputMismatchException e){
+								System.out.println("Not a valid option.");
+								scanner.nextLine();
+							}
+							break;
+						default:
+							IsValid = false;
+							System.out.println("Fun fact: KFC tastes better than BURGER QUEEN...");
+							break;
+					}
+				}while(IsValid);
+			}catch(InputMismatchException e){
+				System.out.println("numbers only");
+				scanner.nextLine();
+		}
 		/*
 		
 		
