dae0d54dc44ada404bb3917da3cce9577ddcb37b
Bryan <brya0021@e.ntu.edu.sg>
2024-04-25 16:16:51 +0800

    Payment?

--- demo/src/test/java/com/PaymentManagement/PaymentMethodFactory.java
+++ demo/src/test/java/com/PaymentManagement/PaymentMethodFactory.java
@@ -1,41 +1,56 @@
 package com.PaymentManagement;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class PaymentMethodFactory {
     
     public Object createPaymentMethod(String className, Object... params) {
         try {
-            Class<?> paymentMethodClass = Class.forName("com.PaymentManagement." + className);
+            // Use Byte Buddy to create a new class implementing the interfaces
+            Class<?> dynamicClass = new ByteBuddy()
+                .subclass(Object.class) // Extend Object class
+                .implement(iPaymentMethod.class, Payable.class) // Implement interfaces
+                .method(ElementMatchers.named("getName"))
+                .intercept(MethodDelegation.to(MyInterceptor.class)) // Implement getName method
+                .method(ElementMatchers.named("processPayment"))
+                .intercept(MethodDelegation.to(MyInterceptor.class)) // Implement processPayment method
+                .make()
+                .load(getClass().getClassLoader())
+                .getLoaded();
             
-            // Get all interfaces implemented by the class
-            Class<?>[] interfaces = paymentMethodClass.getInterfaces();
-            
-            // Check if the class implements both iPaymentMethod and Payable
-            boolean implementsIPayment = false;
-            boolean implementsPayable = false;
-            
-            for (Class<?> intf : interfaces) {
-                if (intf.equals(iPaymentMethod.class)) {
-                    implementsIPayment = true;
-                }
-                if (intf.equals(Payable.class)) {
-                    implementsPayable = true;
-                }
+            // Get constructor matching the parameter types
+            Class<?>[] paramTypes = new Class[params.length];
+            for (int i = 0; i < params.length; i++) {
+                paramTypes[i] = params[i].getClass();
             }
+            Constructor<?> constructor = dynamicClass.getConstructor(paramTypes);
             
-            if (implementsIPayment && implementsPayable) {
-                Constructor<?> constructor = paymentMethodClass.getConstructor(params.getClass());
-                return constructor.newInstance(new Object[]{params});
-            } else {
-                throw new IllegalArgumentException(className + " does not implement both iPaymentMethod and Payable interfaces.");
-            }
+            // Create new instance with constructor and params
+            Object instance = constructor.newInstance(params);
             
-        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException |
-                 IllegalAccessException | InvocationTargetException e) {
+            // Return instance
+            return instance;
+            
+        } catch (NoSuchMethodException | InstantiationException |
+                 IllegalAccessException | java.lang.reflect.InvocationTargetException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+    
+    // Interceptor class to provide method implementations
+    public static class MyInterceptor {
+        
+        public static String getName(Object instance) {
+            return instance.getClass().getName(); // Get class name from the instance
+        }
+        
+        public static boolean processPayment(double amount) {
+            // Return true or false based on payment processing result
+            return true;
         }
     }
 }
